1e5d6ceeba413b7e3a90ec055504d84829110e8a
MoniqueeSilva <moniquedasilva2005@gmail.com>
2026-07-29 20:57:25 -0300

    feat: implementa acesso às casas do tabuleiro

--- src/com/damasdoreino/jogo/Tabuleiro.java
+++ src/com/damasdoreino/jogo/Tabuleiro.java
@@ -1,5 +1,32 @@
 package com.damasdoreino.jogo;
 
 public class Tabuleiro {
-    
+    private final int tamanho;
+    private Casa[][] casas;
+
+    public Tabuleiro() {
+        this.tamanho = 8;
+        this.casas = new Casa[tamanho][tamanho];
+        inicializarCasas();
+    }
+
+    private void inicializarCasas() {
+        for (int linha = 0; linha < tamanho; linha++) {
+            for (int coluna = 0; coluna < tamanho; coluna++) {
+                casas[linha][coluna] = new Casa(linha, coluna);
+            }
+        }
+    }
+
+    private boolean posicaoValida(int linha, int coluna){
+        return linha >= 0 && linha < tamanho &&
+                coluna >= 0 && coluna < tamanho;
+    }
+
+    public Casa getCasa(int linha, int coluna){
+        if(!posicaoValida(linha, coluna)){
+            return null;
+        }
+        return casas[linha][coluna];
+    }
 }
