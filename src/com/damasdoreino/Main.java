d0f175602f56536fe6d6aab705b7d50502851eac
MoniqueeSilva <moniquedasilva2005@gmail.com>
2026-07-30 14:35:21 -0300

    feat: implementa exibição do tabuleiro no console

--- src/com/damasdoreino/Main.java
+++ src/com/damasdoreino/Main.java
@@ -1,5 +1,14 @@
 package com.damasdoreino;
 
+import com.damasdoreino.console.ExibirTabuleiro;
+import com.damasdoreino.jogo.Tabuleiro;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        Tabuleiro tabuleiro = new Tabuleiro();
+        ExibirTabuleiro exibir = new ExibirTabuleiro();
+        exibir.imprimir(tabuleiro);
+
+    }
 }
--- src/com/damasdoreino/console/ExibirTabuleiro.java
+++ src/com/damasdoreino/console/ExibirTabuleiro.java
@@ -1,5 +1,66 @@
 package com.damasdoreino.console;
 
+import com.damasdoreino.enums.CorPeca;
+import com.damasdoreino.jogo.Casa;
+import com.damasdoreino.jogo.Tabuleiro;
+import com.damasdoreino.pecas.Peca;
+
 public class ExibirTabuleiro {
-    
+
+    public void imprimir(Tabuleiro tabuleiro) {
+
+        System.out.println();
+        System.out.println("      0  1  2  3  4  5  6  7");
+        System.out.println("    ┌─────────────────────────┐");
+
+        for (int linha = 0; linha < 8; linha++) {
+
+            System.out.print(" " + linha + "  │ ");
+
+            for (int coluna = 0; coluna < 8; coluna++) {
+
+                Casa casa = tabuleiro.getCasa(linha, coluna);
+
+                if (casa.estaVazia()) {
+
+                    if ((linha + coluna) % 2 == 0) {
+                        System.out.print("⬜ ");
+                    } else {
+                        System.out.print("🟫 ");
+                    }
+
+                } else {
+
+                    Peca peca = casa.getPeca();
+                    System.out.print(obterEmoji(peca) + " ");
+
+                }
+
+            }
+
+            System.out.println("│");
+        }
+
+        System.out.println("    └─────────────────────────┘");
+        System.out.println();
+    }
+
+   private String obterEmoji(Peca peca) {
+    switch (peca.getTipo()) {
+        case SOLDADO:
+            return peca.getCor() == CorPeca.BRANCO ? "⚪" : "⚫";
+
+        case SOLDADO_REAL:
+            return peca.getCor() == CorPeca.BRANCO ? "🤴" : "👸";
+
+        case CAVALEIRO:
+            return peca.getCor() == CorPeca.BRANCO ? "🐴" : "🐎";
+
+        case MAGO:
+            return peca.getCor() == CorPeca.BRANCO ? "🧙" : "🧙‍♂️";
+
+        default:
+            return "❓";
+    }
+}
 }
